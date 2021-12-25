7797c720a9c7193da4d903e7aba0bbbde1ca5794
Arjun Bhobe <arjun.a.bhobe@gmail.com>
2021-12-24 16:57:22 -0800

    Using 13th day instead of 1st

--- practice/1.2.6 (friday)/friday.py
+++ practice/1.2.6 (friday)/friday.py
@@ -15,7 +15,7 @@ fys = []
 
 for i in range(topYear):
     fys.append(1900+i)
-startDay = 2
+thirDay = 0
 
 caln = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 call = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
@@ -23,11 +23,11 @@ call = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 for y in fys:
     if (y % 4 == 0 and y % 100 != 0) or y % 400 == 0:
         cal = call
-    elif y % 4 != 0 or (y % 100 == 0 and y % 400 != 0):
+    else:
         cal = caln
     for mon in cal:
-        d13s[(startDay+12) % 7] += 1
-        startDay = (startDay+mon) % 7
+        d13s[thirDay] += 1
+        thirDay = (thirDay+mon) % 7
 
 for i in range(len(d13s)):
     fout.write(str(d13s[i]))
