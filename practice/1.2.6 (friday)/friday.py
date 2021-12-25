7414f24b36ae51ee010aecee63dfd1ce6e52e64a
Arjun Bhobe <arjun.a.bhobe@gmail.com>
2021-12-24 16:22:32 -0800

    Increased efficiency and fixed divisibility error

--- practice/1.2.6 (friday)/friday.py
+++ practice/1.2.6 (friday)/friday.py
@@ -1,4 +1,8 @@
-
+"""
+ID: arjun.a4
+LANG: PYTHON3
+TASK: friday
+"""
 months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
 d13s = [0,0,0,0,0,0,0]
@@ -13,19 +17,21 @@ for i in range(topYear):
     fys.append(1900+i)
 startDay = 2
 
-for y in fys:
-    for mon in months:
-        d13s[(startDay+12) % 7] += 1
-        if mon == 'Jan' or mon == 'Mar' or mon == 'May' or mon == 'Jul' or mon == 'Aug' or mon == 'Oct' or mon == 'Dec':
-            startDay = (startDay+31) % 7
-        elif mon != 'Feb':
-            startDay = (startDay+30) % 7
-        elif y % 100 == 0 or  y % 4 !=  0:
-            startDay = (startDay + 28) % 7
-        elif y % 4 == 0 and y % 100 != 0:
-            startDay = (startDay + 29) % 7
+caln = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+call = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-for i in d13s:
-    fout.write(str(i)+" ")
+for y in fys:
+    if (y % 4 == 0 and y % 100 != 0) or y % 400 == 0:
+        cal = call
+    elif y % 4 != 0 or (y % 100 == 0 and y % 400 != 0):
+        cal = caln
+    for mon in cal:
+        d13s[(startDay+12) % 7] += 1
+        startDay = (startDay+mon) % 7
+
+for i in range(len(d13s)):
+    fout.write(str(d13s[i]))
+    if i < len(d13s)-1:
+        fout.write(" ")
 fout.write("\n")
 fout.close
